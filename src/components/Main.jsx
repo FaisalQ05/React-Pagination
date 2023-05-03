41d96b988eb7921c570ee3cf1c1d0b5f610272b4
faisalqureshi05 <faisal199605@gmail.com>
2023-05-03 21:05:26 +0500

    sorting table by column

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,12 +1,17 @@
 import React, { useMemo, useState } from "react"
 import { dummyData, dummyDataTwo } from "../assets/data"
 import Pagination from "./Pagination"
+import { FaSort, FaSortDown, FaSortUp } from "react-icons/fa"
 
 const Main = () => {
   const [currentPage, setCurrentPage] = useState(1)
+  const [customPage, setCustomPage] = useState(null)
   const [PageSize, setPageSize] = useState(12)
   const [query, setQuery] = useState("")
-  const [sortQuery, setSortQuery] = useState("")
+  const [sortQuery, setSortQuery] = useState({
+    field: "",
+    dir: "default",
+  })
 
   const getFilteredData = useMemo(() => {
     // console.log("Get filtered data called")
@@ -19,22 +24,29 @@ const Main = () => {
   const getSortedData = useMemo(() => {
     // console.log("Get sorted data called")
     // users.sort((a, b) => a.firstname.localeCompare(b.firstname))
-    if (sortQuery === "fname") {
+    if (sortQuery.field === "fname" && sortQuery.dir === "asc") {
       return [
         ...getFilteredData.sort((a, b) =>
           a.first_name.localeCompare(b.first_name)
         ),
       ]
-    } else if (sortQuery === "fname desc") {
+    } else if (sortQuery.field === "fname" && sortQuery.dir === "dsc") {
       return [
         ...getFilteredData.sort((a, b) =>
           b.first_name.localeCompare(a.first_name)
         ),
       ]
+    } else if (sortQuery.field === "fname" && sortQuery.dir === "default") {
+      // console.log("default sort fname")
+      if (query.length > 0) {
+        return [...getFilteredData]
+      } else {
+        return [...dummyData]
+      }
     } else {
-      return getFilteredData
+      return [...getFilteredData]
     }
-  }, [getFilteredData, sortQuery])
+  }, [getFilteredData, sortQuery, query.length])
 
   const currentTableData = useMemo(() => {
     // console.log("main data")
@@ -43,19 +55,27 @@ const Main = () => {
     return getSortedData.slice(firstPageIndex, lastPageIndex)
   }, [currentPage, PageSize, getSortedData])
 
-  // console.log({ query, sortQuery })
+  // console.log({ sortQuery })
   const onSort = (e) => {
-    setSortQuery(e)
+    setSortQuery({
+      field: "fname",
+      dir:
+        sortQuery.dir === "default"
+          ? "asc"
+          : sortQuery.dir === "asc"
+          ? "dsc"
+          : sortQuery.dir === "dsc" && "default",
+    })
   }
   return (
     <>
-      <button onClick={() => onSort("fname")}>Sort by fname asc</button>
-      <button onClick={() => onSort("fname desc")}>Sort by fname desc</button>
+      {/* <button onClick={(e) => onSort("fname")}>Sort by fname asc</button> */}
+      {/* <button onClick={(e) => onSort("fname desc")}>Sort by fname desc</button> */}
       <div className="container">
         <input
           className="search-field"
           type="text"
-          placeholder="Search ....."
+          placeholder="Search by FIRST NAME"
           onChange={(e) => setQuery(e.target.value)}
         />
         <div className="table-wrapper">
@@ -63,7 +83,24 @@ const Main = () => {
             <thead>
               <tr>
                 <th className="id">ID</th>
-                <th>FIRST NAME</th>
+                <th
+                  onClick={onSort}
+                  style={{
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    cursor: "pointer",
+                  }}
+                >
+                  FIRST NAME{" "}
+                  {sortQuery.dir === "default" ? (
+                    <FaSort />
+                  ) : sortQuery.dir === "asc" ? (
+                    <FaSortUp />
+                  ) : (
+                    <FaSortDown />
+                  )}
+                </th>
                 <th>LAST NAME</th>
                 <th className="email">EMAIL</th>
                 <th>PHONE</th>
@@ -83,7 +120,22 @@ const Main = () => {
               })}
             </tbody>
           </table>
-          <div className="pagination">
+          <div className="pagination-listbar-container">
+            <div className="custom-page-container">
+              <input
+                className="go-to-page-number"
+                type="number"
+                placeholder="Enter Page number"
+                onChange={(e) =>
+                  Number(e.target.value) > 0
+                    ? setCustomPage(Number(e.target.value))
+                    : null
+                }
+              />
+              <button onClick={() => customPage && setCurrentPage(customPage)}>
+                Go
+              </button>
+            </div>
             <Pagination
               className="pagination-bar"
               currentPage={currentPage}
@@ -91,6 +143,7 @@ const Main = () => {
               pageSize={PageSize}
               onPageChange={(page) => setCurrentPage(page)}
             />
+
             <div className="no-of-pages-select">
               <p>No of items per page</p>
               <select
